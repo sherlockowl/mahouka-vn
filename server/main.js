41015bc9109c1e909ebc90d5fd115f711ad19290
KhoaVo <khoavo@v3si.vn>
2016-05-27 15:57:16 +0700

    package and server folder

---
+++ server/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+//var routes = require('./routes');
+//var http = require('http');
+//var path = require('path');
+//var logger = require('morgan');
+//var ECT = require('ect');
+//
+//var app = express();
+//
+//// Set ECT view engine
+//app.set('view engine', 'ect');
+//app.engine('ect', ECT({watch: true, root: __dirname + '/views', ext: '.ect'}).render);
+
+// All environment
